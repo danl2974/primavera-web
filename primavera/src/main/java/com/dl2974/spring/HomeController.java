d32ab798dca455cb3a3a2c0a88137f9821de35cc
DLubin <DLubin@ABO007.welco.local>
2014-05-21 18:48:47 -0400

    May 21 6:48 PM

---
+++ primavera/src/main/java/com/dl2974/spring/HomeController.java
@@ -0,0 +1,47 @@
+package com.dl2974.spring;
+
+import java.text.DateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Handles requests for the application home page.
+ */
+@Controller
+public class HomeController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	
+	/**
+	 * Simply selects the home view to render by returning its name.
+	 */
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String home(Locale locale, Model model) {
+		logger.info("Welcome home! The client locale is {}.", locale);
+		
+		Date date = new Date();
+		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+		
+		String formattedDate = dateFormat.format(date);
+		
+		model.addAttribute("serverTime", formattedDate );
+		
+		Calendar cal = Calendar.getInstance();
+		int second = cal.get(Calendar.SECOND);
+		int milli = cal.get(Calendar.MILLISECOND);
+		int timeproduct = second * milli;
+		
+		model.addAttribute("timeproduct", String.format("Second: %d Milli: %d Product %d", second, milli, timeproduct) );
+		
+		return "home";
+	}
+	
+}
